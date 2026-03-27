f061aded6a5264d17524e2fd6561955e5026d20d
kbb306 <kylerbbelshaw@gmail.com>
2026-03-26 23:25:41 -0600

    Created Database Class

--- app/src/main/java/com/example/nationalpark/ParkDatabase.kt
+++ app/src/main/java/com/example/nationalpark/ParkDatabase.kt
@@ -1,20 +1,13 @@
 package com.example.nationalpark
 
 import androidx.room.Database
-import androidx.room.InvalidationTracker
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 
 @Database(entities = [ Park::class], version=1)
 @TypeConverters(TypeConverter::class)
-class ParkDatabase : RoomDatabase() {
-    override fun createInvalidationTracker(): InvalidationTracker {
-        TODO("Not yet implemented")
-    }
-
-    override fun clearAllTables() {
-        TODO("Not yet implemented")
-    }
+abstract class ParkDatabase : RoomDatabase() {
+    abstract fun dao() : ParkDao
 
 
 }
