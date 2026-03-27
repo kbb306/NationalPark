19b7402ea9081d655f49e4565eb2431c0e778536
kbb306 <kylerbbelshaw@gmail.com>
2026-03-26 23:26:46 -0600

    Filled in ViewModel

--- app/src/main/java/com/example/nationalpark/ParkListViewModel.kt
+++ app/src/main/java/com/example/nationalpark/ParkListViewModel.kt
@@ -1,5 +1,23 @@
 package com.example.nationalpark
 
-class ParkListViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
+class ParkListViewModel : ViewModel() {
+    private val repository : ParkRepository = ParkRepository.get()
+    private val _parks: MutableStateFlow<List<Park>> = MutableStateFlow(emptyList())
+    val parks : StateFlow<List<Park>>
+        get() {
+            return _parks
+        }
+    init {
+        viewModelScope.launch {
+            repository.getParks().collect {
+                _parks.value = it
+            }
+        }
+    }
 }
